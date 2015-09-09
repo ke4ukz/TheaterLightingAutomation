0b49622aa657e4f5d6cd6acb65fd1ae43024638b
Jonathan Dean <ke4ukz@gmx.com>
2015-09-09 12:50:50 -0400

    Updated to version 1.1 (see description)
    * Fades can now be performed simultaneously
    * Exponential and logarithmic fades can now be used for both up and down
    * Added abbreviated function names
    * Fixed exponential fading to use correct equation
    * Steps that produce the same fade value are skipped
    * Constrained fade values to 0-255 to prevent over- and underflows
    * Commands may now end with \n or \r\n
    * "set" command now stores the new value in values[]
    * "get" command now shows pin number
    * "alloff" command now stores the new value in values[] and sets
    fadeMode[] to FADE_NONE for all channels
    * Fixed linearFade to use the same a and b coefficients as everything
    else (instead of m and b)
    * Moved help message to a #define

--- LightFader/LightFader.ino
+++ LightFader/LightFader.ino
@@ -17,12 +17,24 @@
 *************************************************************************/
 
 #define __NAME__ "LightFader"
-#define __VERSION__ "1.0.1"
+#define __VERSION__ "1.1.0"
+
+#define HELPMESSAGE "Commands:\nhelp\nexponential channel,from,to,time\nlogarithmic channel,from,to,time\nlinear channel,from,to,time\nset channel,value\nget channel\nlist\nalloff\nUnambiguous abbreviations are also accepted"
+      
+#define FADE_NONE 0
+#define FADE_LINEAR 1
+#define FADE_EXPONENTIAL 2
+#define FADE_LOGARITHMIC 3
 
 #define NUM_CHANNELS 2
 
 byte channels[] = {3, 5};
 byte values[] = {0, 0};
+byte fadeModes[] = {0, 0};
+unsigned long startTimes[] = {0, 0};
+unsigned long durations[] = {0, 0};
+int aValues[] ={0, 0};
+float bValues[] = {0, 0};
 
 /***************************************************************
 splitInts - Splits a list of numbers separated by a given delimiter into an array of ints 
@@ -60,7 +72,10 @@ setChannel - Sets the PWM value for a channel
 ***************************************************************/
 void setChannel(int channel, int value) {
   if ( (channel >=0) && (channel < NUM_CHANNELS) ) {
+    value = constrain(value, 0, 255);
+    fadeModes[channel] = FADE_NONE; //Turn off any fade effect that may be going on
     analogWrite(channels[channel], value);
+    values[channel] = value;
   }
 }
 
@@ -74,7 +89,9 @@ int getChannel(int channel) {
   if ( (channel >=0) && (channel < NUM_CHANNELS) ) {
     Serial.print("Channel ");
     Serial.print(channel);
-    Serial.print(": ");
+    Serial.print(" (pin ");
+    Serial.print(channels[channel]);
+    Serial.print(") = ");
     Serial.println(values[channel]);
     return values[channel];
   } else {
@@ -84,7 +101,7 @@ int getChannel(int channel) {
 }
 
 /***************************************************************
-linearFade - Fades a channel using a linear curve
+linearFade - Sets fade values for a linear curve
   int channel      The channel to set (between 0 and NUM_CHANNELS-1
   int from         Starting value (0-255)
   int to           Ending value (0-255)
@@ -94,23 +111,13 @@ linearFade - Fades a channel using a linear curve
   Notes:           This method may be used to fade both directions, up and down
 ***************************************************************/
 void linearFade(int channel, int from, int to, int time) {
-  //y = m * x + b
-  float m;
-  unsigned long startTime, x;
-  int y, b;
-  if ( (channel >=0) && (channel < NUM_CHANNELS) ) {
-    if ( (from != to) && (time > 0) ) {
-      m = (to - from) / (float)time;
-      b = from;
-      startTime = millis();
-      do {
-        x = millis() - startTime;
-        y = m * x + b;
-        analogWrite(channels[channel], y);
-      } while (x < time);
-    }
-    analogWrite(channels[channel], to);
-    values[channel] = to;
+  //y = a + b * x
+  if ( (channel >=0) && (channel < NUM_CHANNELS) && (time > 0) ) {
+    aValues[channel] = from;
+    bValues[channel] = (to - from) / (float)time;
+    startTimes[channel] = millis();
+    durations[channel] = time;
+    fadeModes[channel] = FADE_LINEAR;
   }
 }
 
@@ -125,24 +132,18 @@ exponentialFade - Fades a channel using an exponential curve
   Notes:           the value for from must be less than to in order to get a fade
 ***************************************************************/
 void exponentialFade(int channel, int from, int to, int time) {
-  //y = a + b ^ x
-  int a, y;
-  double b;
-  unsigned long startTime, x;
-
-  if ( (channel >=0) && (channel < NUM_CHANNELS) ) {
-    if ( (to > from) && (time > 0) ) {
-      a = from - 1;
-      b = pow( (to - from + 1),  (1 / (double)time) );
-      startTime = millis();
-      do {
-        x = millis() - startTime;
-        y = a + pow(b, x);
-        analogWrite(channels[channel], y);
-      } while (x < time);
-    }
-    analogWrite(channels[channel], to);
-    values[channel] = to;
+  //y = a * b ^ x
+  if ( (channel >=0) && (channel < NUM_CHANNELS) && (time > 0) ) {
+    //This equation won't work with zeroes, so we'll make them ones
+    if (from == 0)
+      from = 1;
+    if (to == 0)
+      to = 1;
+    aValues[channel] = from;
+    bValues[channel] = pow( (to/(float)from), (1.0F / time));
+    startTimes[channel] = millis();
+    durations[channel] = time;
+    fadeModes[channel] = FADE_EXPONENTIAL;
   }
 }
 
@@ -158,27 +159,15 @@ logarithmicFade - Fades a channel using a logarithmic curve
 ***************************************************************/
 void logarithmicFade(int channel, int from, int to, int time) {
   //y = a + b * ln(x)
-  unsigned long startTime, x;
-  int a, y;
-  float b;
-  
-  if ( (channel >=0) && (channel < NUM_CHANNELS) ) {
-    if ( (to < from) && (time > 0) ) {
-      a = from;
-      b = (to - from) / log(time);
-      startTime = millis();
-      do {
-        x = millis() - startTime;
-        if (x != 0) {
-          y = a + b * log(x);
-          analogWrite(channels[channel], y);
-        }
-      } while (x < time);
-      analogWrite(channels[channel], to);
-      values[channel] = to;
-    }
+  if ( (channel >=0) && (channel < NUM_CHANNELS) && (time > 0) ) {
+    aValues[channel] = from;
+    bValues[channel] = (to - from) / log(time);
+    startTimes[channel] = millis();
+    durations[channel] = time;
+    fadeModes[channel] = FADE_LOGARITHMIC;
   }
 }
+
 
 /***************************************************************
 processCommand - Acts according to a command givem
@@ -191,32 +180,34 @@ void processCommand(String command) {
   int spaceAt;
   int argValues[5];
   String task, params;
+  if (command.endsWith("\r") )
+    command = command.substring(0, command.length() - 1);
   spaceAt = command.indexOf(' ');
   if (spaceAt >= 0) {
     task = command.substring(0, spaceAt);
     params = command.substring(spaceAt + 1);
 
-    if (task.equalsIgnoreCase("set") ) {
+    if (task.equalsIgnoreCase("set") || task.equalsIgnoreCase("s") ) {
       if (splitInts(params, ',', 2, argValues) == 2) {
         setChannel(argValues[0], argValues[1]);
       } else {
         Serial.println("Invalid arguments");
       }
-    } else if (task.equalsIgnoreCase("get") ) {
+    } else if (task.equalsIgnoreCase("get") || task.equalsIgnoreCase("g") ) {
       getChannel(params.toInt() );
-    } else if (task.equalsIgnoreCase("exponential") ) {
+    } else if (task.equalsIgnoreCase("exponential") || task.equalsIgnoreCase("e") ) {
       if (splitInts(params, ',', 4, argValues) == 4) {
         exponentialFade(argValues[0], argValues[1], argValues[2], argValues[3]);
       } else {
         Serial.println("Invalid arguments");
       }      
-    } else if (task.equalsIgnoreCase("linear") ) {
+    } else if (task.equalsIgnoreCase("linear") || task.equalsIgnoreCase("lin") ) {
       if (splitInts(params, ',', 4, argValues) == 4) {
         linearFade(argValues[0], argValues[1], argValues[2], argValues[3]);
       } else {
         Serial.println("Invalid arguments");
       }      
-    } else if (task.equalsIgnoreCase("logarithmic") ) {
+    } else if (task.equalsIgnoreCase("logarithmic") || task.equalsIgnoreCase("lo") ) {
       if (splitInts(params, ',', 4, argValues) == 4) {
         logarithmicFade(argValues[0], argValues[1], argValues[2], argValues[3]);
       } else {
@@ -226,28 +217,76 @@ void processCommand(String command) {
       Serial.println("Unknown command");
     }
   } else {
-    if (command.equalsIgnoreCase("help") ) {
-      Serial.println("Commands:");
-      Serial.println("help");
-      Serial.println("exponential channel,from,to,time");
-      Serial.println("logarithmic channel,from,to,time");
-      Serial.println("linear channel,from,to,time");
-      Serial.println("set channel,value");
-      Serial.println("get channel");
-      Serial.println("list");
-      Serial.println("alloff");
-    } else if (command.equalsIgnoreCase("list") ) {
+    if (command.equalsIgnoreCase("help") || command.equalsIgnoreCase("h") ) {
+      Serial.println(HELPMESSAGE);
+    } else if (command.equalsIgnoreCase("list") || command.equalsIgnoreCase("lis") ) {
       for (int i=0; i<NUM_CHANNELS; i++) {
         getChannel(i);
       }
-    } else if (command.equalsIgnoreCase("alloff") ) {
+    } else if (command.equalsIgnoreCase("alloff") || command.equalsIgnoreCase("a") ) {
       for (int i=0; i<NUM_CHANNELS; i++) {
         digitalWrite(channels[i], LOW);
+        values[i] = 0;
+        fadeModes[i] = FADE_NONE;
       }
     } else {
       Serial.println("Unknown command");
     }
   }
+}
+
+/***************************************************************
+fadeStep - Calculates and applies the next lighting step value for the given channel
+  int channel      The channel to step
+  
+  Returns:         None
+***************************************************************/
+void fadeStep(int channel) {
+  int y;
+  unsigned long x;
+  
+  //Quit early if we don't fade this channel
+  if (fadeModes[channel] == FADE_NONE)
+    return;
+    
+  x = millis() - startTimes[channel];
+  if (x > durations[channel]) {
+    //We've run out of time, so make sure it's set to the full value
+    x = durations[channel];
+  }
+
+  switch(fadeModes[channel]) {
+    case FADE_LINEAR:
+      // y = a + b * x
+      y = aValues[channel] + bValues[channel] * x;
+      break;
+    case FADE_EXPONENTIAL:
+      //y = a * b ^ x
+     y = aValues[channel] * pow(bValues[channel], x);
+      break;
+    case FADE_LOGARITHMIC:
+      //y = a + b * ln(x)
+      y = aValues[channel] + bValues[channel] * log(x);
+      break;
+    default: //Shouldn't happen, but saves us from setting an invalid y-value in case it does
+      return;
+      break;
+  }
+
+  if (x == durations[channel]) {
+    //Last iteration, so turn off further fading
+    fadeModes[channel] = FADE_NONE;
+  }
+  
+  //Make sure we didn't go over or under the valid range for byte
+  y = constrain(y, 0, 255);
+
+  //Don't change the value if it's the same thing to reduce flickering
+  if (y != values[channel]) {
+    analogWrite(channels[channel], y);
+    values[channel] = (byte)y;
+  }
+
 }
 
 /***************************************************************
@@ -275,7 +314,10 @@ loop - Runs continually as long as the device is powered on
   Notes:           This method is called again as soon as it is finished, thus running the code in it over and over
 ***************************************************************/
 void loop() {
-  //Nothing done in here, everything is driven from data arriving over the serial port
+  //Go through each channel and step its fade (if necessary)
+  for (int i=0; i<NUM_CHANNELS; i++) {
+    fadeStep(i);
+  }
 }
 
 /***************************************************************
